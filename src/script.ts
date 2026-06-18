e1662e61e01fea93d1f1836899ef4c44a6a4e4e1
MD Soriful Islam <islamsoriful.hero@gmail.com>
2026-06-18 08:48:33 +0600

    Basics prisma Create a new user with a post

--- src/script.ts
+++ src/script.ts
@@ -1,41 +1,52 @@
+import { create } from "node:domain";
 import { prisma } from "./lib/prisma";
 
 async function main() {
-  // Create a new user with a post
-  const user = await prisma.user.create({
-    data: {
-      name: "Alice",
-      email: "alice@prisma.io",
-      posts: {
-        create: {
-          title: "Hello World",
-          content: "This is my first post!",
-          published: true,
+    // Create a new user with a post
+    // const user = await prisma.user.create({
+    //     data : {
+    //        name : "Soriful",
+    //        email : "soriful@prisma.io",
+    //        posts : {
+    //             create : {
+    //                 title : "Hello World 2",
+    //                 content : "This is my second post as 2nd user!",
+    //                 published : true,
+    //             }
+    //        }
+    //     },
+    //     include : {
+    //         posts : true,
+    //     }
+    // })
+
+
+    const newPost = await prisma.post.create({
+        data: {
+            authorId: 1,
+            title: "Hello World 2",
+            content: "This is my second post as 1st user!",
+            published: true,
         },
-      },
-    },
-    include: {
-      posts: true,
-    },
-  });
+    });
+    console.log("Created post:", newPost);
+    // console.log("Created user:", user);
 
-  console.log("Created user:", user);
-
-  // Fetch all users with their posts
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-    },
-  });
-  console.log("All users:", JSON.stringify(allUsers, null, 2));
+    // Fetch all users with their posts
+    const allUsers = await prisma.user.findMany({
+        include: {
+            posts: true,
+        },
+    });
+    console.log("All users:", JSON.stringify(allUsers, null, 2));
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
